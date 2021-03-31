725765e57ade550826d441885de01a774a597534
svjatber <svjat.ber3003@gmail.com>
2021-04-01 01:33:54 +0300

    "good"

--- app.js
+++ app.js
@@ -2,17 +2,28 @@
 const app = Vue.createApp({
     data(){
         return {
-           name: 'Svjatoslav',
-           age: 20,
-           img: 'http://qnimate.com/wp-content/uploads/2014/03/images2.jpg'
+         counter: 0,
+         name: '',
+         confirmedName:'svj'
         }
     },
-    methods: {
-        getCourseGoal() {
-            return this.courseGoal
+    methods:{
+        setConfirmedName(){
+            this.confirmedName = this.name
+        },
+        submitForm(){
+           alert('Submitted')
+        },
+        add(num){
+            this.counter += num
+        },
+        reduce(num){
+            this.counter -= num
+        },
+        setName(event, lastName){
+            this.name = event.target.value + ' ' + lastName
         }
     }
-
 })
 
-app.mount('#app')
+app.mount('#events')

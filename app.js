a88d84f8e58955cd2914ce9e7f0a13463fc495ec
svjatber <svjat.ber3003@gmail.com>
2021-04-02 14:26:18 +0300

    'done'

--- app.js
+++ app.js
@@ -8,15 +8,43 @@ const app = Vue.createApp({
   methods: {
     saveInput(event) {
       this.currentUserInput = event.target.value;
+      console.log(this.currentUserInput)
     },
     setText() {
-      // this.message = this.currentUserInput;
-      console.dir(this.$refs.userText)
+        this.message = this.currentUserInput
     },
   },
+  beforeCreate(){
+      console.log('beforeCreate')
+  },
+  created(){
+      console.log('created')
+  },
+  beforeMount(){
+      console.log('beforeMount')
+  },
+  mounted(){
+      console.log('mounted')
+  },
+  beforeUpdate(){
+      console.log('beforeUpdate')
+  },
+  updated(){
+      console.log('updated')
+  },
+  beforeUnmount(){
+      console.log('beforeUnMount')
+  },
+  unmounted(){
+      console.log('unmounted')
+  }
 });
 
 app.mount('#app');
+
+setTimeout(()=>{
+    app.unmount()
+}, 3000)
 
 const app2 = Vue.createApp({
     data() {

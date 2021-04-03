8b706b8b26aa68224b5583681239399f8bd3c09b
svjatber <72857388+svjatber@users.noreply.github.com>
2021-04-03 13:18:56 +0300

    Add files via upload
    Provide/inject

---
+++ src/src/components/KnowledgeGrid.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <knowledge-element
+      v-for="topic in topics1"
+      :key="topic.id"
+      :id="topic.id"
+      :topic-name="topic.title"
+      :description="topic.description"
+      @select-topic="$emit('select-topic', $event)"
+    ></knowledge-element>
+  </ul>
+</template>
+
+<script>
+export default {
+  inject: ['topics1'],
+  emits: ['select-topic'],
+  created () {
+    console.log(this.topics)
+  }
+}
+</script>

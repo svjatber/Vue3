cf72d74919e663a40e0e9c03a9eb5cf9cfb3b0ed
svjatber <72857388+svjatber@users.noreply.github.com>
2021-04-03 15:38:30 +0300

    Add files via upload
    Provide

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <base-card>
+    <template #main>
+      <the-header></the-header>
+      <badge-list></badge-list>
+    </template>
+    <template v-slot:second>
+      <user-info
+        :full-name="activeUser.name"
+        :info-text="activeUser.description"
+        :role="activeUser.role"
+      ></user-info>
+    </template>
+  </base-card>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader'
+import BadgeList from './components/BadgeList.vue'
+import UserInfo from './components/UserInfo.vue'
+import BaseCard from './components/BaseCard'
+
+export default {
+  components: {
+    BaseCard,
+    TheHeader,
+    BadgeList,
+    UserInfo
+  },
+  data () {
+    return {
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>

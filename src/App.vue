cf1f97911be1fe6c442a18d105ab6aade34fe25a
svjatber <72857388+svjatber@users.noreply.github.com>
2021-04-04 15:25:38 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+  <the-form></the-form>
+  </div>
+</template>
+
+<script>
+import TheForm from './components/TheForm'
+export default {
+  data () {
+    return { }
+  },
+  components: {
+    TheForm
+  }
+}
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
